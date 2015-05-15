8c55541eeeb006294b9a27a73f7cc714ce4181e5
Hyun Jun, An <dltn123@gmail.com>
2015-05-15 16:44:05 +0900

    Init profile logic

---
+++ app/profile/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .views import profile
+
+# Process Login, Logout, Register, Register Apply
---
+++ app/profile/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from flask import Blueprint
+
+profile = Blueprint('profile', __name__, template_folder='templates')
